1c7c27ae6704e0476e8106410907c939a9c2dc77
Huangbin <huangbin@haodf.com>
2018-03-01 17:55:08 +0800

    fix wrong title

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ class Site(object):
     def writeHtml(self, body, title, filepath):
         html = self.readHtmlTemp()
         html = html.replace('{{article}}', body)
-        html = html.replace('{{title}}', article.title)
+        html = html.replace('{{title}}', title)
         html = html.replace('{{categories}}', self.getCategoryHTML())
         html = html.replace('{{update-time}}',time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
         dir = os.path.dirname(HTML_DIR + filepath)
